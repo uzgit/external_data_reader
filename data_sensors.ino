20133980a00aeb430e40cde4a23340101b33558c
uzgit <jspri17@gmail.com>
2016-11-26 23:14:09 -0600

    Added final code.

--- data_sensors.ino
+++ data_sensors.ino
@@ -9,7 +9,7 @@ dht DHT;
 #define ONE_WIRE_BUS 4
 #define VOLTAGE_PIN 0
 #define CURRENT_PIN 1
- 
+#define CURRENT_RTRNS 50
 // Setup a oneWire instance to communicate with any OneWire devices 
 // (not just Maxim/Dallas temperature ICs)
 OneWire oneWire(ONE_WIRE_BUS);
@@ -28,12 +28,14 @@ struct Data
   long  battery_status;
 };
 
+float previous_current;
 Data data;
 
 void setup()
 {
   Wire.begin(7);
   Wire.onRequest(WriteEvent);
+  Serial.begin(9600);
 }
 
 void loop()
@@ -41,12 +43,39 @@ void loop()
     // READ DATA
   int chk = DHT.read(DHT22_PIN);
   data.air_temperature = DHT.temperature;
-  data.humidity = DHT.humidity;
-   
+  data.humidity = DHT.humidity; 
   sensors.requestTemperatures(); // Send the command to get temperatures
-
   data.water_temperature = sensors.getTempCByIndex(0);
-  delay(10);
+
+  int rtrn = analogRead(VOLTAGE_PIN);
+  float voltage = ((rtrn/1024.0)) * 5;
+  //Serial.println(voltage);
+  voltage = voltage * 5 + 0.5;
+  //Serial.println(voltage);
+  data.voltage = voltage;
+
+
+  float sum = 0.0;
+  int total = 0;
+  float current = 0.0;
+  
+  for(int i =0; i<CURRENT_RTRNS; i++)
+  {
+    rtrn = analogRead(CURRENT_PIN);
+    if(rtrn!=0 && rtrn!=1023)
+     {
+      sum+=((rtrn/1024.0)) * 500;
+      total++;
+     }
+  }
+  if(total!=0)
+    current = sum / total;
+
+  data.current = current;  
+  //Serial.println(current);
+
+  
+  delay(300);
 }
 
 void WriteEvent()
